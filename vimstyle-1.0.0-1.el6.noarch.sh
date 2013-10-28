16cf2ae80a6cd3eb3da9d61a212febbc4d43d0f5
Clumart.G <myregs6@gmail.com>
2013-10-28 15:10:36 +0000

    create project

---
+++ vimstyle-1.0.0-1.el6.noarch.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+################ Script Info ################		
+
+## Program: Auto Script V1.0
+## Author:Clumart.G
+## Date: 2013-12-12
+## Update:None
+
+
+################ Env Define ################
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/sbin
+LANG=C
+export PATH
+export LANG
+
+################ Var Setting ################
+
+InputVar=$*
+HomeDir="/tmp/autoscript/"
+
+################ Func Define ################ 
+function _info_msg() {
+_header
+echo -e " |                                                                |"
+echo -e " |                Thank you for use this script!                  |"
+echo -e " |                                                                |"
+echo -e " |                         Version: 1.0                           |"
+echo -e " |                                                                |"
+echo -e " |                     http://www.idcsrv.com                      |"
+echo -e " |                                                                |"
+echo -e " |                   Author:翅儿学飞(Clumart.G)                   |"
+echo -e " |                    Email:myregs6@gmail.com                     |"
+echo -e " |                         QQ:1810836851                          |"
+echo -e " |                         QQ群:61749648                          |"
+echo -e " |                                                                |"
+echo -e " |          Hit [ENTER] to continue or ctrl+c to exit             |"
+echo -e " |                                                                |"
+printf " o----------------------------------------------------------------o\n"	
+ read entcs 
+clear
+}
+
+function _end_msg() {
+echo -e "###################################################################"
+echo ""
+echo -e "                         Install Finish :)"
+echo ""
+echo -e "###################################################################"
+echo ""
+echo ""
+_header
+echo -e " |                                                                |"
+echo -e " |                 Thank you for use this script!                 |"
+echo -e " |                                                                |"
+echo -e " |                The software has been installed!                |"
+echo -e " |                                                                |"
+echo -e " |                     http://www.idcsrv.com                      |"
+echo -e " |                                                                |"
+echo -e " |                   Author:翅儿学飞(Clumart.G)                   |"
+echo -e " |                    Email:myregs6@gmail.com                     |"
+echo -e " |                         QQ:1810836851                          |"
+echo -e " |                         QQ群:61749648                          |"
+echo -e " |                                                                |"
+printf " o----------------------------------------------------------------o\n"
+}
+
+function _header() {
+	printf " o----------------------------------------------------------------o\n"
+	printf " | :: Auto Script                             v1.0.0 (2012/12/12) |\n"
+	printf " o----------------------------------------------------------------o\n"	
+}
+
+##Program Function
+
+################ Main ################
+clear
+_info_msg
+
+if [ `id -u` != "0" ]; then
+echo -e "You need to be be the root user to run this script.\nWe also suggest you use a direct root login, not su -, sudo etc..."
+exit 1
+fi
+
+if [ ! -d $HomeDir ]; then
+	mkdir -p $HomeDir
+fi
+
+cd $HomeDir || exit 1
+
+
+_end_msg
+############  Clean Cache  ############
+rm -rf ${HomeDir}
